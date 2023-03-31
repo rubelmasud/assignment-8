42629e38dd4402c6c3e95fbaa8065786badc14ec
MD:Rubrl RM <inforubel251@gmail.com>
2023-03-31 17:56:30 +0600

    added click handller

--- src/components/main-section/Main.jsx
+++ src/components/main-section/Main.jsx
@@ -19,7 +19,18 @@ const Main = () => {
                 }
             </div>
             <div className="add-cart lg:w-3/12 bg-gray-50 ">
+                <div className="total-min-container my-4 bg-gray-50 h-12 rounded text-center">
+                    <h4 className='text-2xl font-semibold text-blue-700'>Spent time on read:min</h4>
+                </div>
+                <div className="total-min-container bg-gray-50 rounded px-4">
+                    <div className="blogs-quantity ">
+                        <h4 className='text-2xl font-semibold'>Bookmarked Blogs : </h4>
+                    </div>
+                    <div className="title-cart m-6 p-3 rounded bg-white">
+                        jteyje6
+                    </div>
 
+                </div>
             </div>
         </div>
     );
